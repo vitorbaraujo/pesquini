667c6a86fe878dba83b5339970dc29f5ba183620
João Guilherme <joaaogui@gmail.com>
2015-04-25 11:31:18 -0300

    Criando migrations de state, sanction_typee payments

---
+++ pesquini/db/migrate/0004_create_sanction_type.rb
@@ -0,0 +1,9 @@
+class CreateSanctionType < ActiveRecord::Migration 
+	def change
+		create_table :sanctiontype do |t|
+			t.string :type
+			t.string :legal_foundation
+			t.string :foundation_description
+		end
+	end
+end
