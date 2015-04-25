cdaaf39a47c1bebedeef6adcb75d7d25544009e1
Alexandre <alexandrekry@gmail.com>
2015-04-25 12:25:54 -0300

    Ferramenta de busca, a finalizar

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -1,7 +1,13 @@
 class EnterprisesController < ApplicationController
   def index
+
+  	if params[:search]
+  		@enterprise = Enterprise.search(params[:search])
+    	@title = "Resultados para: <br/>".html_safe + "'#{params[:search]}'"
+    end
+
     @enterprises = Enterprise.all
-
-
   end
+
+
  end
