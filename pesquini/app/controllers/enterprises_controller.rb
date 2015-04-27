e173ea8b4260c7e8045eb4f11178f25b0d777a2b
Gustavo <gustavo.mn@outlook.com>
2015-04-26 21:15:10 -0300

    Gem Ransack instalada e a função pesquisar pronta

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -1,13 +1,6 @@
 class EnterprisesController < ApplicationController
   def index
-
-    if params[:search]
-          @enterprise = Enterprise.find_by_corporate_name(params[:search])
-          @title = "Resultados para: <br/>".html_safe + "'#{params[:search]}'"
-    end
-
-    @enterprises = Enterprise.all
+    @search = Enterprise.search(params[:q])
+    @enterprises = @search.result
   end
-
-
  end
