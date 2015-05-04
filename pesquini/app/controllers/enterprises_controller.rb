76218e605becef6ba7bb81e1eea298e2e328b570
Alexandre T. K <alexandrekry@gmail.com>
2015-05-04 20:53:35 -0300

    - Inserindo busca na Home Page (welcome) - Adicionando metodo show das enterprises - Link da entreprise-index para a enterprise-show

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -1,9 +1,32 @@
 class EnterprisesController < ApplicationController
   def index
-    params[:q][:cnpj_eq] = params[:q][:corporate_name_cont]
 
-    @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
-    puts params ,"asa"*20 
-    @enterprises = @search.result
+    if params[:q].nil?
+
+      @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
+
+    else
+      params[:q][:cnpj_eq] = params[:q][:corporate_name_cont]
+
+      @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
+
+      puts params ,"asa"*20 
+      @enterprises = @search.result
+    end
+
+
   end
+
+  def show
+    @enterprise = Enterprise.find(params[:id])
+  end
+
  end
+
+
+ #=======
+ #     @search = Enterprise.search(params[:q])
+ #   unless params[:q].nil?
+ #     @enterprises = @search.result
+ #   end
+ #>>>>>>> Stashed changes
