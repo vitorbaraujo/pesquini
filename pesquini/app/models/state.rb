513fa1558f9c6f50e896e389d936a0c2d93d64dc
Gustavo <gustavo.mn@outlook.com>
2015-05-07 19:04:30 -0300

    removendo metodo desnecessario

--- pesquini/app/models/state.rb
+++ pesquini/app/models/state.rb
@@ -2,10 +2,6 @@ class State < ActiveRecord::Base
   has_many :sanctions
   validates_uniqueness_of :abbreviation
 
-  def id
-    self.id
-  end
-
   def refresh!
     s = State.find_by_abbreviation(self.abbreviation)
   end
