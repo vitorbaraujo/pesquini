61393bbd4c1e840f1009f90a31bd82e2f8758463
Gustavo <gustavo.mn@outlook.com>
2015-05-07 20:26:42 -0300

    Graficos funcionando com o novo layout

--- pesquini/app/controllers/graphics_controller.rb
+++ pesquini/app/controllers/graphics_controller.rb
@@ -1,11 +1,24 @@
 class GraphicsController < ApplicationController
   @@states_list = ["BA", "DF", "RJ", "PA", "MG", "SP", "AM", "RS", "SC", "ES", "PR", 
-                   "PB", "RN", "CE", "AL", "RR", "SE", "RO", "Não Informado", "AC", 
-                   "TO", "GO", "PE", "AP", "MS", "MT", "MA", "PI"]
+                   "PB", "RN", "CE", "AL", "RR", "SE", "RO","PI" , "AC", 
+                   "TO", "GO", "PE", "AP", "MS", "MT", "MA","Não Informado"]
 
   def index
     gon.states = @@states_list
     gon.dados = total_by_state
+    @titulo = "Grafico de Sançoes por Estado"
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+    f.title(:text => "Graficos de Sançoes por Estado")
+    f.xAxis(:categories => @@states_list)
+    f.series(:name => "Numero de Sançoes", :yAxis => 0, :data => total_by_state)
+    f.yAxis [
+    {:title => {:text => "Sançoes", :margin => 70} },
+    {:title => {:text => "Sançoes"}, :opposite => true},
+    ]
+
+    f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+    f.chart({:defaultSeriesType=>"column"})
+end
   end
 
   def percentual_sanction(value)
@@ -18,7 +31,7 @@ class GraphicsController < ApplicationController
     @@states_list.each do |s|
       state = State.find_by_abbreviation("#{s}")
       sanctions_by_state = Sanction.where(state_id: state[:id])
-    @results << percentual_sanction(sanctions_by_state.count)
+    @results << (sanctions_by_state.count)
     end
     @results
   end
