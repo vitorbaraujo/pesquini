35466e91547a425930745b0289e1482df77efc61
Alexandre T. K <alexandrekry@gmail.com>
2015-05-07 12:47:06 -0300

    Alteração do layout para a primeira release (remoção dos componentes que não serão utilizados)

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -4,6 +4,7 @@ class EnterprisesController < ApplicationController
     if params[:q].nil?
 
       @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
+      @enterprises = Enterprise.all
 
     else
       params[:q][:cnpj_eq] = params[:q][:corporate_name_cont]
