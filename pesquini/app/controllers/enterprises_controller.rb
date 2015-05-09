0576dc0dd58229e6073a230e522a93c01d5afc1b
Alexandre T. K <alexandrekry@gmail.com>
2015-05-09 19:10:17 -0300

    - Adicionando paginação para a aplicação - Removendo turbolink Rails ( estava bugando javascript ) - Arrumando links afetados pelo merge

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -2,11 +2,11 @@ class EnterprisesController < ApplicationController
   def index
     if params[:q].nil?
       @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
-      @enterprises = Enterprise.all
+      @enterprises = Enterprise.paginate(:page => params[:page], :per_page => 10)
     else
       params[:q][:cnpj_eq] = params[:q][:corporate_name_cont]
       @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
-      @enterprises = @search.result
+      @enterprises = @search.result.paginate(:page => params[:page], :per_page => 10)
     end
   end
 
