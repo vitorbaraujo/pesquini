2a85de4c71b0485e1ab9ccfd3a68bd804ac7648b
Ruan Donato <ruandonato@outlook.com>
2015-05-23 16:12:54 -0300

    Criando metodos em statistics controller

--- pesquini/app/controllers/statistics_controller.rb
+++ pesquini/app/controllers/statistics_controller.rb
@@ -1,4 +1,4 @@
-class StatisticController < ApplicationController
+class StatisticsController < ApplicationController
 
   def  index
 #    @enterprises = Enterprise.featured
@@ -9,8 +9,32 @@ class StatisticController < ApplicationController
   end
 
   def sanction_by_state_graph
+    gon.states = @@states_list
+    gon.dados = total_by_state
+    @titulo = "Gráfico de Sanções por Estado"
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+    f.title(:text => "Gráficos de Sanções por Estado")
+    f.xAxis(:categories => @@states_list)
+    f.series(:name => "Numero de Sanções", :yAxis => 0, :data => total_by_state)
+    f.yAxis [
+    {:title => {:text => "Sanções", :margin => 70} },
+    {:title => {:text => "Sanções"}, :opposite => true},
+    ]
+
+    f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+    f.chart({:defaultSeriesType=>"column"})
+end
 
   end
 
+  def total_by_state
+    @results = []
+    @@states_list.each do |s|
+      state = State.find_by_abbreviation("#{s}")
+      sanctions_by_state = Sanction.where(state_id: state[:id])
+    @results << (sanctions_by_state.count)
+    end
+    @results
+  end
 
 end
