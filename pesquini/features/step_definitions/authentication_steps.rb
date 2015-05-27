bc1c563985e53b44d611d7af431637f09b270fa4
Ruan Donato <ruandonato@outlook.com>
2015-05-27 10:44:11 -0300

    Finalizando teste funcional enterprises/index

--- pesquini/features/step_definitions/authentication_steps.rb
+++ pesquini/features/step_definitions/authentication_steps.rb
@@ -3,15 +3,12 @@
 Dado(/^que a empresa samsung exista$/) do
   @enterprise = Enterprise.new
   @enterprise.corporate_name = "samsung"
+  @enterprise.cnpj = "555"
   @enterprise.save
 end
 
 Dado(/^que eu estou na index de enterprises$/) do
   visit '/enterprises'
-end
-
-Dado(/^que eu clico no text field$/) do
-  pending # express the regexp above with the code you wish you had
 end
 
 Dado(/^que insiro uma empresa existente$/) do
@@ -22,12 +19,17 @@ Quando(/^eu clico no botão "(.*?)"$/) do |arg1|
   click_button arg1
 end
 
-Entao(/^vejo o texto os dados desta empresa\.$/) do
-  page.should have_content('samsung')
+Entao(/^vejo todos os dados desta empresa\.$/) do
+page.should have_content('samsung')
 end
+
 
 Dado(/^que eu estou na index da welcome$/) do
   visit '/'
+end
+
+Entao(/^vejo os dados prévios desta empresa\.$/) do
+page.should have_content('555')
 end
 
 Quando(/^eu clico no link "(.*?)"$/) do |arg1|
