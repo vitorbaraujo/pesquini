aab52cc38dd0f8165c83f41bf74b01b7418ac3c0
Gustavo Moreira <gustavomn93@gmail.com>
2015-05-30 16:15:39 -0300

    Alterando labels e corrigndo gráfico quando paramentro for nulo

--- pesquini/app/controllers/graphics_controller.rb
+++ pesquini/app/controllers/graphics_controller.rb
@@ -28,12 +28,17 @@ end
       state = State.find_by_abbreviation("#{s}")
       sanctions_by_state = Sanction.where(state_id: state[:id])
       selected_year = []
+      if(params[:year_].to_i != 0)
         sanctions_by_state.each do |s|
           if(s.initial_date.year ==  params[:year_].to_i)
             selected_year << s
           end
         end
         @results << (selected_year.count)
+      else
+        @results << (sanctions_by_state.count)
+      end
+
     end
   @results
   end
