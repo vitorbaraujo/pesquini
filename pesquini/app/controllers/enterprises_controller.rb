36a758d20ecba54baf4c63790b19a2b55bfbd991
Victor Henrique <victinhohenrique@gmail.com>
2015-06-04 00:04:07 -0300

    Adicionando o campo posicao no ranking na view de enterprises

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -14,5 +14,17 @@ class EnterprisesController < ApplicationController
     @enterprise = Enterprise.find(params[:id])
     @collection = Sanction.where(enterprise_id: @enterprise.id)
     @sanctions = @collection.paginate(:page => params[:page], :per_page => 10)
+    @position = enterprise_position(@enterprise)
+  end
+
+  def enterprise_position(enterprise)
+
+      a = Enterprise.all.sort_by{|x| x.sanctions_count}
+      b = a.uniq.group_by(&:sanctions_count).to_a.reverse
+
+      xd = 0
+      b.each_with_index{|k,index|
+      xd = index if k[1].first.sanctions_count == enterprise.sanctions_count}
+    return xd +1
   end
  end
