b7cbc58ed73e70c661dfa6e0574378a9effd1a23
Ruan Donato <ruandonato@outlook.com>
2015-06-05 15:04:44 -0300

    Aumentando cobertura de teste rspec

--- pesquini/spec/controllers/statistics_controller_spec.rb
+++ pesquini/spec/controllers/statistics_controller_spec.rb
@@ -2,11 +2,33 @@ require 'rails_helper'
 
 RSpec.describe StatisticsController, :type => :controller do 
   describe "GET" do 
+    
+    describe '#sanction_by_state_graph' do
+      it "should work" do
+        get :sanction_by_state_graph
+        expect(response).to have_http_status(:success)
+      end
+    end
+
     describe '#index' do
-      it "statistics/index" do
+      it "should work" do
         get :index
         expect(response).to have_http_status(:success)
       end
     end    
+
+    describe '#most_sanctioned_ranking' do
+      it "should work" do
+        get :most_sanctioned_ranking
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe '#sanction_by_type_graph' do
+      it "should work" do
+        get :sanction_by_type_graph
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
