61cfa256a62c30126454d62d342d0b770e0568d4
Ruan Donato <ruandonato@outlook.com>
2015-06-05 15:04:44 -0300

    Aumentando cobertura de teste rspec

--- pesquini/spec/controllers/enterprises_controller_spec.rb
+++ pesquini/spec/controllers/enterprises_controller_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe EnterprisesController, :type => :controller do 
-    describe   "GET" do 
-      describe '#index' do
-        it "should work" do 
-          get :index
+  before do
+    @enterprise = Enterprise.create(cnpj: "12345")
+  end
+
+  describe "GET" do 
+    describe '#index' do
+      it "should work" do 
+        get :index
         expect(response).to have_http_status(:success)
+      end
+    end
+    
+    describe '#show' do
+      describe 'with a registered enterprise' do 
+        it "should work" do
+          get :show, :id => @enterprise.id
+          expect(response).to have_http_status(:success)
+        end
       end
     end
   end
