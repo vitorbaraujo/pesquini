da3f969c5221f85a98160c5bb05831b1051794db
João Guilherme <joaaogui@gmail.com>
2015-06-06 15:16:40 -0300

    Modificando Session controller e excluindo testes da pasta

--- pesquini/spec/controllers/sessions_controller_spec.rb
+++ pesquini/spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do 
-	describe   "GET" do 
-		describe '#new' do
-			it "should work" do 
-				get :new
-				expect(response).to have_http_status(:success)
-			end
-		end
-	end
+
+  user = User.new(login: 'sanjaninha', password: 'sanjana123')
+  user.save
+
+  subject { post :create, :session => {:login => 'sanjana', :password => 'sanjana123'}}
+  describe   "GET" do 
+    describe '#create' do
+      it "should log in user with correct login and password" do 
+        post :create, :session => {:login =>"sanjaninha", :password => 'sanjana123'}
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "should redirect log in page if user does not exist" do 
+        post :create, :session => {:login => 'anything', :password => 'shit'}
+        expect(response).to render_template("/new")
+      end
+    end
+  end
 end
