015514e230f6f97c565a52a577a382c3279c60a7
Ruan Donato <ruandonato@outlook.com>
2015-06-06 15:20:19 -0300

    Refatorando o sessions controller e escrevendo teste

--- pesquini/app/controllers/sessions_controller.rb
+++ pesquini/app/controllers/sessions_controller.rb
@@ -1,20 +1,28 @@
 class SessionsController < ApplicationController
-	def new
-	end
-	def create
-		user = User.find_by(login: params[:session][:login].downcase)
-  		if user && user.authenticate(params[:session][:password])
-  			sign_in user
-  			redirect_to root_path
-  		else
-			  flash[:error] = 'Invalid combination.'
-			  render 'new'
-  		end	
-	end
+
+  def new
+
+  end
+
+  def create
+    login = params[:session][:login].downcase
+    password = params[:session][:password]
+    user = User.find_by(login: login)
+
+    if user && user.authenticate(password)
+      puts "bla"
+      sign_in user
+      redirect_to root_path
+    else
+      flash[:error] = 'Invalid combination.'
+      render :new
+    end
+  end
+
   def destroy
     if signed_in?
       sign_out 
       redirect_to root_path
     end
   end
- end
+end
--- pesquini/spec/factories.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    login                 "sanjana"
-    password              "sanjana123"
-    password_confirmation "sanjana123"
-  end
-end
