05a7ef738f3d36d3f9dd25e02e033f3c05d11caf
Alexandre T. K <alexandrekry@gmail.com>
2015-06-08 16:01:38 -0300

    Arrumando bug do javascript(página de search), estava afetando outros JS...

--- pesquini/app/assets/javascripts/search.js
+++ pesquini/app/assets/javascripts/search.js
@@ -1,10 +1,13 @@
  $(document ).ready(function ( ) {
-  var element = document.getElementById('enterprises_index').getElementsByClassName("fa_search")[0];
-  var search1 = document.getElementById('enterprises_index').getElementsByClassName("search_form_enterprises")[0];
-
+  var element = document.getElementById('enterprises_index')
+  if (element)
+  {
+  var element1 = element.getElementsByClassName("fa_search")[0];
+  var search1 = element.getElementsByClassName("search_form_enterprises")[0];
+  }
   $(element).on('click', function () {
     $(search1).removeClass("off");
-    $(element).parent().addClass('off');
+    $(element1).addClass('off');
   });
 
 
