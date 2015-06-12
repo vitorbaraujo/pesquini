efb145e30ae6feda940f572599421a93b74e1a53
Alexandre T. K <alexandrekry@gmail.com>
2015-06-11 22:45:18 -0300

    Adicionando botão de voltar nas subcategorias de sancoes

--- pesquini/app/assets/javascripts/enterprises.js
+++ pesquini/app/assets/javascripts/enterprises.js
@@ -1,19 +1,19 @@
  $(document ).ready(function ( ) {
 
 if(document.URL.indexOf("#tf-sanction_by_state") != -1) {
-  $("#tf-sanction_by_state").css("background-color","black");
+  $("#tf-sanction_by_state").css("background-color","#101010");
   $("#tf-sanction_by_state").fadeOut(1);
   $("#tf-sanction_by_state").fadeIn(1500);    
 }
 
 if(document.URL.indexOf("#tf-most_sanctioned_ranking") != -1) {
-  $("#tf-most_sanctioned_ranking").css("background-color","black");
+  $("#tf-most_sanctioned_ranking").css("background-color","#101010");
   $("#tf-most_sanctioned_ranking").fadeOut(1);
   $("#tf-most_sanctioned_ranking").fadeIn(1500);    
 }
 
 if(document.URL.indexOf("#tf-sanction_by_type") != -1) {
-  $("#tf-sanction_by_type").css("background-color","black");
+  $("#tf-sanction_by_type").css("background-color","#101010");
   $("#tf-sanction_by_type").fadeOut(1);
   $("#tf-sanction_by_type").fadeIn(1500);    
 }
