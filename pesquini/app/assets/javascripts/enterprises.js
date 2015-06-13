5a676d424728330c0c6f03c48d03394997d68b84
Alexandre T. K <alexandrekry@gmail.com>
2015-06-13 18:41:30 -0300

    Consertando bug na controller estatisticas com relaçao ao vetor dos estados, organizando controller, mudando a cor na index da statistics via javascript

--- pesquini/app/assets/javascripts/enterprises.js
+++ pesquini/app/assets/javascripts/enterprises.js
@@ -2,18 +2,21 @@
 
 if(document.URL.indexOf("#tf-sanction_by_state") != -1) {
   $("#tf-sanction_by_state").css("background-color","#101010");
+  $("#tf-sanction_by_state").css("color","#F7F7F7");
   $("#tf-sanction_by_state").fadeOut(1);
   $("#tf-sanction_by_state").fadeIn(1500);    
 }
 
 if(document.URL.indexOf("#tf-most_sanctioned_ranking") != -1) {
   $("#tf-most_sanctioned_ranking").css("background-color","#101010");
+  $("#tf-most_sanctioned_ranking").css("color","#F7F7F7");
   $("#tf-most_sanctioned_ranking").fadeOut(1);
   $("#tf-most_sanctioned_ranking").fadeIn(1500);    
 }
 
 if(document.URL.indexOf("#tf-sanction_by_type") != -1) {
   $("#tf-sanction_by_type").css("background-color","#101010");
+  $("#tf-sanction_by_type").css("color","#F7F7F7");
   $("#tf-sanction_by_type").fadeOut(1);
   $("#tf-sanction_by_type").fadeIn(1500);    
 }
--- pesquini/app/controllers/statistics_controller.rb
+++ pesquini/app/controllers/statistics_controller.rb
@@ -1,5 +1,8 @@
 class StatisticsController < ApplicationController
 
+######################################################
+# Declarações de variáveis Globais
+
 @@states_list = ["BA", "DF", "RJ", "PA", "MG", "SP", "AM", "RS", "SC", "ES", "PR", 
                    "PB", "RN", "CE", "AL", "RR", "SE", "RO","PI" , "AC", 
                    "TO", "GO", "PE", "AP", "MS", "MT", "MA","Não Informado"]
@@ -7,63 +10,6 @@ class StatisticsController < ApplicationController
 @@sanjana = ["Todos",1988, 1991, 1992, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 
              2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 
              2014, 2015] 
-
-
-
-  def  index
-  end
-
-  def most_sanctioned_ranking
-    @enterprises = Enterprise.featured
-  end
-
-  def sanction_by_state_graph
-    gon.states = @@states_list
-    gon.dados = total_by_state
-    @titulo = "Gráfico de Sanções por Estado"
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
-    f.title(:text => "Gráficos de Sanções por Estado")
-    if(params[:year_].to_i != 0)
-       f.title(:text => params[:year_].to_i ) 
-     end
-    f.xAxis(:categories => @@states_list)
-    f.series(:name => "Numero de Sanções", :yAxis => 0, :data => total_by_state)
-    f.yAxis [
-    {:title => {:text => "Sanções", :margin => 70} },
-    {:title => {:text => "Sanções"}, :opposite => true},
-    ]
-    f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
-    f.chart({:defaultSeriesType=>"column"})
- 
-end
-
-  end
-
-  def total_by_state
-    @results = []
-    @years = @@sanjana
-    @@states_list.each do |s|
-      state = State.find_by_abbreviation("#{s}")
-      sanctions_by_state = Sanction.where(state_id: state[:id])
-      selected_year = []
-      if(params[:year_].to_i != 0)
-        sanctions_by_state.each do |s|
-          if(s.initial_date.year ==  params[:year_].to_i)
-            selected_year << s
-          end
-        end
-        @results << (selected_year.count)
-      else
-        @results << (sanctions_by_state.count)
-      end
-
-    end
-  @results
-  end
-
-
-@@sanction_type_list = Hash.new
-
 
 @@sanction_type_list = [
  [ "INIDONEIDADE - LEGISLAçãO ESTADUAL", "Inidoneidade - Legislação Estadual"], 
@@ -84,52 +30,109 @@ end
  ]
 
 
+######################################################
+# Métodos da controller
+
+  def  index
+  end
+
+  def most_sanctioned_ranking
+    @enterprises = Enterprise.featured
+  end
+
+  def sanction_by_state_graph
+    gon.states = @@states_list
+    gon.dados = total_by_state
+    titulo = "Gráfico de Sanções por Estado"
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(:text => titulo)
+      if(params[:year_].to_i != 0)
+         f.title(:text => params[:year_].to_i ) 
+       end
+      f.xAxis(:categories => @@states_list)
+      f.series(:name => "Número de Sanções", :yAxis => 0, :data => total_by_state)
+      f.yAxis [
+      {:title => {:text => "Sanções", :margin => 30} },
+      ]
+      f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+      f.chart({:defaultSeriesType=>"column"})
+    end
+  end
+
 
   def sanction_by_type_graph
-titulo = "Gráfico Sanções por Tipo"
-
-@chart = LazyHighCharts::HighChart.new('pie') do |f|
-
-      f.chart({:defaultSeriesType=>"pie" ,:margin=> [50, 10, 10, 10]} )
-      f.series({
-               :type=> 'pie',
-               :name=> 'Sanções Encontradas',
-               :data => total_by_type
-      })
-      f.options[:title][:text] = titulo
-      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto', :right=> '50px', :top=> '100px'}) 
-      f.plot_options(:pie=>{
-        :allowPointSelect=>true, 
-        :cursor=>"pointer" , 
-        :dataLabels=>{
-          :enabled=>true,
-          :color=>"black",
-          :style=>{
-          :font=>"12px Trebuchet MS, Verdana, sans-serif"
+    titulo = "Gráfico Sanções por Tipo"
+    @chart = LazyHighCharts::HighChart.new('pie') do |f|
+        f.chart({:defaultSeriesType=>"pie" ,:margin=> [50, 10, 10, 10]} )
+        f.series({
+                 :type=> 'pie',
+                 :name=> 'Sanções Encontradas',
+                 :data => total_by_type
+        })
+        f.options[:title][:text] = titulo
+        f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto', :right=> '50px', :top=> '100px'}) 
+        f.plot_options(:pie=>{
+          :allowPointSelect=>true, 
+          :cursor=>"pointer" , 
+          :dataLabels=>{
+            :enabled=>true,
+            :color=>"black",
+            :style=>{
+            :font=>"12px Trebuchet MS, Verdana, sans-serif"
+            }
           }
-        }
-      })
-end
-    respond_to do |format|
-    format.html # show.html.erb
-    format.js
-end
+        })
+    end
 
-end
-
-def percentual_sanction(value)
-    total = Sanction.all.count
-    value * 100 / total
-end
-
-def total_by_type
-    @results = []
-    results2 = []
-    cont = 0
-    @states = @@states_list
-    if @states[0] != "Todos"
+    if (!@states)
+      @states = @@states_list.clone
       @states.unshift("Todos")
     end
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js
+    end
+
+  end
+
+
+######################################################
+# Métoodos auxiliares
+
+  def percentual_sanction(value)
+      total = Sanction.all.count
+      value * 100 / total
+  end
+
+
+  
+  def total_by_state()
+    @results = []
+    @years = @@sanjana
+    @@states_list.each do |s|
+      state = State.find_by_abbreviation("#{s}")
+      sanctions_by_state = Sanction.where(state_id: state[:id])
+      selected_year = []
+      if(params[:year_].to_i != 0)
+        sanctions_by_state.each do |s|
+          if(s.initial_date.year ==  params[:year_].to_i)
+            selected_year << s
+          end
+      end
+        results << (selected_year.count)
+      else
+        results << (sanctions_by_state.count)
+      end
+    end
+    results
+  end
+
+
+  def total_by_type()
+    results = []
+    results2 = []
+    cont = 0
 
     state = State.find_by_abbreviation(params[:state_])
 
@@ -140,10 +143,10 @@ def total_by_type
         sanctions_by_type = sanctions_by_type.where(state_id: state[:id])
       end
       cont = cont + (sanctions_by_type.count)
-    results2 << s[1]
-    results2 << (sanctions_by_type.count)
-    @results << results2
-    results2 = []
+      results2 << s[1]
+      results2 << (sanctions_by_type.count)
+      results << results2
+      results2 = []
     end
     results2 << "Não Informado" 
       if (params[:state_] && params[:state_] != "Todos")
@@ -152,8 +155,10 @@ def total_by_type
         total = Sanction.count
       end
     results2 << (total - cont)
-    @results << results2
-    @results = @results.sort_by { |i| i[0] }
+    results << results2
+    results = results.sort_by { |i| i[0] }
+    results
   end
+
 
 end
