7b5b7b45001ff7f70eecc61ceed4104454b72076
Marcelo Martins <martins.oliveira.mo@gmail.com>
2015-06-14 14:42:57 -0300

    Substituindo metodo deprecated no teste

--- pesquini/spec/controllers/sessions_controller_spec.rb
+++ pesquini/spec/controllers/sessions_controller_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, :type => :controller do 
+RSpec.describe SessionsController, :type => :controller do
 
   user = User.new(login: 'sanjaninha', password: 'sanjana123')
   user.save
 
   subject { post :create, :session => {:login => 'sanjana', :password => 'sanjana123'}}
-  describe   "GET" do 
+  describe   "GET" do
     describe '#create' do
-      it "should log in user with correct login and password" do 
+      it "should log in user with correct login and password" do
         post :create, :session => {:login =>"sanjaninha", :password => 'sanjana123'}
         expect(response).to redirect_to(root_path)
       end
     end
   end
 
-  describe "#destroy" do  
+  describe "#destroy" do
     it "should sign out the user" do
       post :create, :session => {:login =>"sanjaninha", :password => 'sanjana123'}
       get :destroy
-      session[:user_id].should be(nil)
+      expect(session[:user_id]).to be(nil)
       expect(response).to redirect_to(root_path)
     end
   end
