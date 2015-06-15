bf6933882a15973dc3d0293a332527fd3d9ed770
João Guilherme <joaaogui@gmail.com>
2015-06-15 15:22:50 -0300

    Iniciando estrutura para visualização de informação na show

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -18,10 +18,11 @@ class EnterprisesController < ApplicationController
     @payments = Payment.where(enterprise_id: @enterprise.id).paginate(:page => params[:page], :per_page => @per_page )
     @sanctions = @collection.paginate(:page => params[:page], :per_page => @per_page)
     @position = enterprise_position(@enterprise)
+    @p_collection = Payment.where(enterprise_id: @enterprise.id)
+    @payments = @p_collection.paginate(:page => params[:page], :per_page => 10)
   end
 
   def enterprise_position(enterprise)
-
       a = Enterprise.all.sort_by{|x| x.sanctions_count}
       b = a.uniq.group_by(&:sanctions_count).to_a.reverse
 
