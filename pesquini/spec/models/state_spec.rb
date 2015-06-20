46659311f81ab0756b7bc81ec4d20ace71339872
Ruan Donato <ruandonato@outlook.com>
2015-06-20 09:58:25 -0300

    Testando atributo abbreviation da model state unitariamente

---
+++ pesquini/spec/models/state_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe State do
+
+  before do
+    @state = State.new
+    @state.abbreviation = "DF1"
+    @state.save
+  end
+
+  subject {@state}
+    it { should respond_to(:abbreviation) }
+    it { should respond_to(:id) }
+    it { should respond_to(:code) }
+    it { should respond_to(:name) }
+  it { should be_valid }
+
+  describe "uniqueness validation of abbreviation" do
+    
+    describe "unique abbreviation" do
+      it "should be_valid" do
+        uniqueness_state = State.new
+        uniqueness_state.abbreviation = "SP1"
+        uniqueness_state.save 
+        expect(uniqueness_state).to be_valid
+      end
+    end
+
+   describe "duplicated abbreviation" do
+        it "should not be_valid" do
+        duplicated_state = State.new
+        duplicated_state.abbreviation = "DF1" 
+        expect(duplicated_state).not_to be_valid
+      end
+    end
+    
+  end
+end
