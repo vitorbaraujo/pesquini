9c9a35db914584a377e709474ca6441a252a7114
Renata Soares <renata.sds09@gmail.com>
2016-06-17 16:54:04 -0300

    Inserting comments for payment controller

--- pesquini/app/controllers/parser/parser_payment_controller.rb
+++ pesquini/app/controllers/parser/parser_payment_controller.rb
@@ -1,32 +1,54 @@
+# File: parser_payment_controller.rb
+# Purpose: Does the parser with government contracting data.
+# License: LGPL. No copyright.
+
+# This class of the controller sets the behavior for parsing government contracting data
+
 class Parser::ParserPaymentController < Parser::ParserController
 
   require 'csv'
   require 'open-uri'
+  # Path to file for parser
   @@filename = 'parser_data/CEIS.csv'
 
-  before_filter :authorize, only: [:check_nil_ascii, :check_date, :import, 
-                                       :build_state, :build_sanction_type, 
-                                       :build_enterprise, :build_sanction, 
-                                       :check_and_save]
+  # Specify that the filter should be applied to the given controller actions
+  before_filter :authorize, only: [:check_nil_ascii, 
+                                    :check_date, 
+                                    :import, 
+                                    :build_state, 
+                                    :build_sanction_type, 
+                                    :build_enterprise, 
+                                    :build_sanction, 
+                                    :check_and_save]
 
-  def index
-
-  end
+  # name: check_value
+  # explanation: This method recieves a string representing a payment value in 
+  # and make a convertion of type.
+  # parameters:
+  # - text
+  # return: the number in float type or nil in case of fail.
 
   def check_value(text)
-    #this method recieves a string representing a payment value in the format 19,470.99
-    #Then it takes off the comma (",") and parse it to float format as 19470.99
-     begin
+    # receiveis a string in the format 19,470.99. Then it takes off the comma (",") and 
+    # parse it to float format as 19470.99.
+    begin
       return text.gsub(",","").to_f
     rescue
       return nil
     end
   end
 
-  def import()
+  # name: import
+  # explanation: This method recieves the data of the file.
+  # parameters:
+  # - none
+  # return: an Payment.
+
+  def import
     constante = 0
     Enterprise.find_each do |e|
 
+      # Url where the government data is hosted
       url = 'http://compras.dados.gov.br/contratos/v1/contratos.csv?cnpj_contratada='
       begin
         data =  open(url+e.cnpj).read
@@ -52,6 +74,12 @@ class Parser::ParserPaymentController < Parser::ParserController
     puts "="*50
     puts "Quantidade de empresas sem pagamentos: ", constante
   end
+
+  # name: check_and_save
+  # explanation: This method checks the data received and save it.
+  # parameters:
+  # - c
+  # return: a C.
 
   def check_and_save(c)
     begin
