52d20301224b94a4c4bcba4c7a2c6ed41cee12fe
Vítor Barbosa <vitornga15@gmail.com>
2016-06-22 09:13:09 -0300

    Insert file and method comments for application helper

--- pesquini/app/helpers/application_helper.rb
+++ pesquini/app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
+# File: application_helper.rb
+# Purpose: Implementation of the Application Helper
+# License: LGPL. No copyright.
+
+# This helper contains auxiliar methods to be used in the whole application.
+
 module ApplicationHelper
+
+  # name: flash_class
+  # explanation: this method returns a class for each type of flash message
+  # parameters:
+  # - level: symbol to inform type of flash message
+  # return: string
   def flash_class(level)
     case level
         when :notice then "alert alert-info"
@@ -7,4 +19,5 @@ module ApplicationHelper
         when :alert then "alert alert-error"
     end
   end
+
 end
