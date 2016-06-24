8b4fd985d74bf8d94f1e3dec46744009c19d8a2f
Vítor Barbosa <vitornga15@gmail.com>
2016-06-24 11:13:30 -0300

    Uncomplicating code for application helper

--- pesquini/app/helpers/application_helper.rb
+++ pesquini/app/helpers/application_helper.rb
@@ -9,20 +9,24 @@ module ApplicationHelper
   # name: flash_class
   # explanation: this method returns a class for each type of flash message
   # parameters:
-  # - level: symbol to inform type of flash message
+  # - type_of_flash_message: symbol to inform type of flash message
   # return: string
-  def flash_class(level)
-    if level == :notice
-      "alert alert-info"
-    elsif level == :success
-      "alert alert-success"
-    elsif level == :error
-      "alert alert-error"
-    elsif level == :alert
-      "alert alert-error"
+  def flash_class(type_of_flash_message)
+    returned_class = ""
+
+    if type_of_flash_message == :notice
+      returned_class = "alert alert-info"
+    elsif type_of_flash_message == :success
+      returned_class = "alert alert-success"
+    elsif type_of_flash_message == :error
+      returned_class = "alert alert-error"
+    elsif type_of_flash_message == :alert
+      returned_class = "alert alert-error"
     else
       # nothing to do
     end
+
+    return returned_class
   end
 
 end
