b2de104617a59924a969f97fbe8c29831b744f96
jessicacris <jessica.cris1127@gmail.com>
2016-06-25 12:54:38 -0300

    Checking default behavior for application controller

--- pesquini/app/controllers/application_controller.rb
+++ pesquini/app/controllers/application_controller.rb
@@ -5,15 +5,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
 
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
-  rescue_from ActionController::RoutingError, :with => :render_not_found
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    rescue_from ActionController::RoutingError, :with => :render_not_found
 
   def raise_not_found!
-    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+      raise ActionController::RoutingError.new("No route matches \
+      #{params[:unmatched_route]}")
   end
 
   def render_not_found
-    respond_to do |render_not_found|
+      respond_to do |render_not_found|
       render_not_found.html{ render :template => "errors/404", :status => 404 }
     end
   end
