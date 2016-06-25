dc1ba833b37a44ba518d108be133a7fe5752c713
jessicacris <jessica.cris1127@gmail.com>
2016-06-25 13:09:56 -0300

    Checking default behavior for state model

--- pesquini/app/models/state.rb
+++ pesquini/app/models/state.rb
@@ -3,14 +3,17 @@ class State < ActiveRecord::Base
   validates_uniqueness_of :abbreviation
 
   def refresh!
-    sanctions = State.find_by_abbreviation(self.abbreviation)
+      actual_sanction = State.find_by_abbreviation( self.abbreviation )
+      
+      return actual_sanction
   end
 
   def self.all_states
     states = ["BA", "DF", "RJ", "PA", "MG", "SP", "AM", "RS", "SC", "ES", "PR",
      "PB", "RN", "CE", "AL", "RR", "SE", "RO","PI" , "AC",
-    "TO", "GO", "PE", "AP", "MS", "MT", "MA","Não Informado"]
-    states
+     "TO", "GO", "PE", "AP", "MS", "MT", "MA","Não Informado"]
+    
+    return states
   end
 
 end
