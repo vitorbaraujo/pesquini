92c1b5a432775cf3534a00bc51b45dc4269417eb
jessicacris <jessica.cris1127@gmail.com>
2016-06-26 20:12:41 -0300

    Inserting assertives for user model

--- pesquini/app/models/user.rb
+++ pesquini/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	include Assertions
+	extend Assertions
 
 	has_secure_password
 	validates :login, length: { maximum: 50, minimum: 5 },
@@ -8,15 +10,16 @@ class User < ActiveRecord::Base
 	length: { minimum: 8 },
 	allow_blank: false
 
-	def User.new_remember_token
-  		secure_token = SecureRandom.urlsafe_base64
-			return secure_token
+  def User.new_remember_token
+  	secure_token = SecureRandom.urlsafe_base64
+	return secure_token
   end
 
 
   def User.digest(token)
   	encripted_token = Digest::SHA1.hexdigest( token.to_s )
-
   	return encripted_token
- 	end
+  end
+
 end
+ 
