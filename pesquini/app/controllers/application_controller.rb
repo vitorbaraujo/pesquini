1155a5930fe91469d92e86cbff8c313cc1b5de0b
jessicacris <jessica.cris1127@gmail.com>
2016-06-26 19:43:19 -0300

    Inserting assertives for application controller

--- pesquini/app/controllers/application_controller.rb
+++ pesquini/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  include Assertions
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -16,6 +17,7 @@ class ApplicationController < ActionController::Base
   def render_not_found
       respond_to do |render_not_found|
       render_not_found.html{ render :template => "errors/404", :status => 404 }
+      assert__render_not_found (render)
     end
   end
 
