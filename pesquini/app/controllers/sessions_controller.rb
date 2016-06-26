890e3a522e48599538702ae5bfe87da6c6f974f8
jessicacris <jessica.cris1127@gmail.com>
2016-06-26 19:52:34 -0300

    Inserting assertives for sessions controller

--- pesquini/app/controllers/sessions_controller.rb
+++ pesquini/app/controllers/sessions_controller.rb
@@ -5,9 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    
     login = params[:session][:login].downcase
+    assert_object_is_not_null (login)
     password = params[:session][:password]
+    assert_object_is_not_null (password)
     user = User.find_by(login: login)
+    assert_object_is_not_null (user)
 
     if (user && user.authenticate(password))
       sign_in user
@@ -20,6 +24,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    
     if (signed_in?)
       sign_out 
     return redirect_to root_path
