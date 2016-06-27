f74c3d7edda0f98531a02b61e67fecdc8a71bb8f
Renata Soares <renata.sds09@gmail.com>
2016-06-27 02:32:53 -0300

    Adding tests for statistics controller

--- pesquini/test/controllers/statistics_controller_test.rb
+++ pesquini/test/controllers/statistics_controller_test.rb
@@ -1,2 +1,33 @@
 class StatisticsControllerTest < ActionController::TestCase
+
+    test "should get index" do
+        get :index
+
+        assert :success
+    end
+
+    test "should get template for index" do
+        get :index
+
+        assert_template :index
+    end
+
+    test "should get most most_sanctioned_ranking" do
+        get :most_sanctioned_ranking
+
+        assert :success
+    end
+
+    test "should get template for most_sanctioned_ranking" do
+        get :most_sanctioned_ranking
+
+        assert_template :most_sanctioned_ranking
+    end
+
+    test "should get most_paymented_ranking" do
+        get :most_paymented_ranking
+
+        assert :success
+    end
+
 end
