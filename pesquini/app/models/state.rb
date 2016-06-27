5715ec18409a5dfb0787d43881489ceb8d95f19c
jessicacris <jessica.cris1127@gmail.com>
2016-06-26 21:24:13 -0300

    Inserting comments for state model

--- pesquini/app/models/state.rb
+++ pesquini/app/models/state.rb
@@ -1,8 +1,21 @@
+#  File: state
+#  Purpose: Implementation of state model 
+#  License : LGPL. No copyright.
+
+# This controller represents states in the Brazilian territory
+
+
 class State < ActiveRecord::Base
   include Assertions
 
   has_many :sanctions
   validates_uniqueness_of :abbreviation
+
+# name: update_state
+# explanation: this method refreshes a given state.
+# parameters:
+# - non
+# return: actual_sanction
 
   def update_state
       actual_sanction = State.find_by_abbreviation( self.abbreviation )
@@ -10,6 +23,12 @@ class State < ActiveRecord::Base
       
     return actual_sanction
   end
+
+# name: self.all_states
+# explanation: this method prepares all state's abbreviations.
+# parameters:
+# - non
+# return: states
 
   def self.all_states
     states = ["BA", "DF", "RJ", "PA", "MG", "SP", "AM", "RS", "SC", "ES", "PR",
