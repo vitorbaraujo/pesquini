a9661dc56a7aeb753fabfae41f20812cec054a29
Vítor Barbosa <vitornga15@gmail.com>
2016-06-27 02:34:18 -0300

    Adding more tests for statistics controller

--- pesquini/test/controllers/statistics_controller_test.rb
+++ pesquini/test/controllers/statistics_controller_test.rb
@@ -30,4 +30,46 @@ class StatisticsControllerTest < ActionController::TestCase
         assert :success
     end
 
+    test "should get template for most_paymented_ranking" do
+        get :most_paymented_ranking
+
+        assert_template :most_paymented_ranking
+    end
+
+    test "should get enterprise_group_ranking" do
+        get :enterprise_group_ranking
+
+        assert :success
+    end
+
+    test "should get template enterprise_group_ranking" do
+        get :enterprise_group_ranking
+
+        assert_template :enterprise_group_ranking
+    end
+
+    test "should get sanction_by_state_graph" do
+        get :sanction_by_state_graph
+
+        assert :success
+    end
+
+    test "should get template sanction_by_state_graph" do
+        get :sanction_by_state_graph
+
+        assert_template :sanction_by_state_graph
+    end
+
+    test "should get sanction_by_type_graph" do
+        get :sanction_by_type_graph
+
+        assert :success
+    end
+
+    test "should get template sanction_by_type_graph" do
+        get :sanction_by_type_graph
+
+        assert_template :sanction_by_type_graph
+    end
+
 end
